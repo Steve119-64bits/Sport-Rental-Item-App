99b66a00cf3002fb74fdd84bdb4cd8d7585b8735
Chua Yun Sheng <141478528+Chua123Yun@users.noreply.github.com>
2025-05-05 09:40:07 +0800

    Create imageMap.ts

---
+++ imageMap.ts
@@ -0,0 +1,11 @@
+const imageMap: { [key: string]: any } = {
+  wood_paddle: require('./assets/wood_paddle.png'),
+  graphite_paddle: require('./assets/graphite_paddle.png'),
+  composite_paddle: require('./assets/composite_paddle.png'),
+  outdoor_pickleball: require('./assets/outdoor_pickleball.png'), 
+  indoor_pickleball: require('./assets/indoor_pickleball.png'),  
+  nylon_net: require('./assets/nylon_net.png'),
+  polyethylene_net: require('./assets/polyethylene_net.png'),
+};
+
+export default imageMap;
